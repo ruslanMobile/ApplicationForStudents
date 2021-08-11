34fa40df963bb663b4af0ea06acd79129aa24d74
Ruslan <ruslanosadtsia@gmail.com>
2021-08-11 15:48:44 +0300

    Add activity fot save profile settings

--- app/src/main/java/com/example/applicationforstudents/AboutMeActivity.java
+++ app/src/main/java/com/example/applicationforstudents/AboutMeActivity.java
@@ -2,23 +2,34 @@ package com.example.applicationforstudents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AboutMeActivity extends AppCompatActivity {
-    Button button;
+    ImageButton buttonBack,buttonEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
-        button = findViewById(R.id.mmm);
-        button.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().hide();
+
+        buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("MyLog","==============");
                 finish();
+            }
+        });
+        buttonEditText = findViewById(R.id.buttonEditText);
+        buttonEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),EditAboutMeActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/applicationforstudents/EditAboutMeActivity.java
@@ -0,0 +1,14 @@
+package com.example.applicationforstudents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditAboutMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_about_me);
+    }
+}
