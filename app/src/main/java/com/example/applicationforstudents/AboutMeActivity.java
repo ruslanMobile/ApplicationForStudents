5ba697099da1de6f3e06be095889db16bdd751f2
Ruslan <ruslanosadtsia@gmail.com>
2021-08-13 14:42:13 +0300

    Finished activity about me

--- app/src/main/java/com/example/applicationforstudents/AboutMeActivity.java
+++ app/src/main/java/com/example/applicationforstudents/AboutMeActivity.java
@@ -3,28 +3,41 @@ package com.example.applicationforstudents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class AboutMeActivity extends AppCompatActivity {
     ImageButton buttonBack,buttonEditText;
+    TextView textViewEditGroup,textViewEditCourse,textViewEditDirection,textViewEditStudentCard,textViewEditProfTicket;
+    private static final String PREFS_FILE = "Profile";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
         getSupportActionBar().hide();
 
+        textViewEditGroup = findViewById(R.id.textViewEditGroup);
+        textViewEditCourse = findViewById(R.id.textViewEditCourse);
+        textViewEditDirection = findViewById(R.id.textViewEditDirection);
+        textViewEditStudentCard = findViewById(R.id.textViewEditStudentCard);
+        textViewEditProfTicket = findViewById(R.id.textViewEditProfTicket);
+
         buttonBack = findViewById(R.id.buttonBack);
+        //Обробка натиску на вихід з актівіті
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
         buttonEditText = findViewById(R.id.buttonEditText);
+        //Перехід в актівіті для зміни даних студента
         buttonEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,6 +45,19 @@ public class AboutMeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    //Відображення даних студента з пам'яті
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_FILE,MODE_PRIVATE);
+        String str = "";
+        textViewEditGroup.setText((str = sharedPreferences.getString("GroupName","Не указано")).trim().equals("") ? "Не указано" : str);
+        textViewEditCourse.setText((str = sharedPreferences.getString("CourseName","Не указано")).trim().equals("") ? "Не указано" : str);
+        textViewEditDirection.setText((str = sharedPreferences.getString("DirectionName","Не указано")).trim().equals("") ? "Не указано" : str);
+        textViewEditStudentCard.setText((str = sharedPreferences.getString("StudentCardName","Не указано")).trim().equals("") ? "Не указано" : str);
+        textViewEditProfTicket.setText((str = sharedPreferences.getString("ProfTicketName","Не указано")).trim().equals("") ? "Не указано" : str);
     }
 
 }
--- app/src/main/java/com/example/applicationforstudents/EditAboutMeActivity.java
+++ app/src/main/java/com/example/applicationforstudents/EditAboutMeActivity.java
@@ -2,13 +2,73 @@ package com.example.applicationforstudents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class EditAboutMeActivity extends AppCompatActivity {
+    ImageButton buttonBackEdit;
+    Button buttonSave;
+    SharedPreferences sharedPreferences;
+    EditText editTextGroup,editTextCourse,editTextDirection,editTextStudentCard,editTextProfTicket;
+    private static final String PREFS_FILE = "Profile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_about_me);
+        getSupportActionBar().hide();
+
+        sharedPreferences = getSharedPreferences(PREFS_FILE,MODE_PRIVATE);
+
+        editTextGroup = findViewById(R.id.editTextGroup);
+        editTextCourse = findViewById(R.id.editTextCourse);
+        editTextDirection = findViewById(R.id.editTextDirection);
+        editTextStudentCard = findViewById(R.id.editTextStudentCard);
+        editTextProfTicket = findViewById(R.id.editTextProfTicket);
+        initEditText();
+
+        buttonBackEdit = findViewById(R.id.buttonBackEdit);
+        //Обробка натиску на вихід з актівіті
+        buttonBackEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Изменения не сохранены",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        buttonSave = findViewById(R.id.buttonSaveAboutMe);
+        buttonSave.setOnClickListener(clickButtonSave);
     }
+    //Ініціалізація полів для вводу з пам'яті
+    public void initEditText(){
+        editTextGroup.setText(sharedPreferences.getString("GroupName",""));
+        editTextCourse.setText(sharedPreferences.getString("CourseName",""));
+        editTextDirection.setText(sharedPreferences.getString("DirectionName",""));
+        editTextStudentCard.setText(sharedPreferences.getString("StudentCardName",""));
+        editTextProfTicket.setText(sharedPreferences.getString("ProfTicketName",""));
+    }
+
+    //Обробка натиску на збереження даних про студента
+    View.OnClickListener clickButtonSave = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            SharedPreferences.Editor prefEdit = sharedPreferences.edit();
+            prefEdit.putString("GroupName",editTextGroup.getText().toString());
+            prefEdit.putString("CourseName",editTextCourse.getText().toString());
+            prefEdit.putString("DirectionName",editTextDirection.getText().toString());
+            prefEdit.putString("StudentCardName",editTextStudentCard.getText().toString());
+            prefEdit.putString("ProfTicketName",editTextProfTicket.getText().toString());
+            prefEdit.apply();
+            Toast.makeText(getApplicationContext(),"Изменения сохранены",Toast.LENGTH_SHORT).show();
+            Log.d("MyLog","test");
+            finish();
+        }
+    };
 }
